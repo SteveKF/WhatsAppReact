478fd548cf74282de35d9fa2e5f164b0cefe5382
Stefan Wohlfart <stefan.wohlfart@aracom.de>
2019-12-04 10:25:25 +0100

    add  screens and components

---
+++ src/screens/ChatViewScreen.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const ChatViewScreen = () => {
+    return (
+        <View style={{ height: 100, width: 100, backgroundColor: 'red' }}>
+            <Text>Chat View Screen</Text>
+        </View>
+    )
+}
---
+++ src/screens/Conversations.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Text, View } from 'react-native'
+
+export const ConversationsScreen = () => {
+    return (
+        <View style={{ height: 100, width: 100, backgroundColor: 'red' }}>
+            <Text>Conversations Screen</Text>
+        </View>
+    )
+}
